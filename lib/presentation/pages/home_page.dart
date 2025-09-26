83aaf6117c58c77e33a974cc2e0006b3755a5e87
Leo <leonardonifinluri@gmail.com>
2025-09-26 22:42:37 +0800

    feat: using contact provider in home page

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,10 +1,13 @@
+import 'package:contact_app/presentation/providers/contact_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = context.watch<ContactProvider>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Contact App'),
@@ -12,8 +15,29 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(16),
-        child: Center(child: Text('Contact App Page')),
+        child: Column(
+          children: [
+            if (provider.isLoading) const CircularProgressIndicator(),
+            if (provider.error != null) Text('Error: ${provider.error}'),
+            provider.contacts.isEmpty
+                ? Text('No contacts found')
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: provider.contacts.length,
+                      itemBuilder: (context, index) {
+                        final contact = provider.contacts[index];
+                        return ListTile(
+                          title: Text(contact.name),
+                          subtitle: Text(contact.phoneNumber),
+                        );
+                      },
+                    ),
+                  ),
+          ],
+        ),
       ) /* Placeholder is contact list */,
+      floatingActionButton: FloatingActionButton(onPressed: null, child: Icon(Icons.add),),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
