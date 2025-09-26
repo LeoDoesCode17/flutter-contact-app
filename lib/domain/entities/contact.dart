a88e77bdb3cfbe8391fefd66197bd1ce767851d5
Leo <leonardonifinluri@gmail.com>
2025-09-26 17:41:45 +0800

    feat: add convert to map and create contact from map function for contact entity

--- lib/domain/entities/contact.dart
+++ lib/domain/entities/contact.dart
@@ -2,14 +2,34 @@ import 'package:contact_app/domain/entities/contact_gender.dart';
 
 class Contact {
   final int id;
+  final String name;
   final String phoneNumber;
   final Gender gender;
   final String description;
 
   const Contact({
     required this.id,
+    required this.name,
     required this.phoneNumber,
     required this.gender,
     required this.description,
   });
+
+  // Convert Contact to Map
+  Map<String, Object?> toMap() => {
+    'id': id,
+    'name': name,
+    'phone_number': phoneNumber,
+    'gender': gender,
+    'description': description,
+  };
+
+  // Create contact from Map (warning to parse data of gender)
+  factory Contact.fromMap(Map<String, Object?> map) => Contact(
+    id: map['id'] as int,
+    name: map['name'] as String,
+    phoneNumber: map['phone_number'] as String,
+    gender: map['gender'] as Gender,
+    description: map['description'] as String,
+  );
 }
